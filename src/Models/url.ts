ec89dbb9585a89520520767f6d4ba6d984fb4304
Roland Daynauth <daynauth@gmail.com>
2023-10-18 10:49:44 -0400

    mod

--- src/Models/url.ts
+++ src/Models/url.ts
@@ -1,1 +1,2 @@
-export const URL = "https://translation-backend.csclarke.com/api/"
+export const URL = "https://141.212.106.68:8000/api/"
+//export const URL = "https://translation-backend.csclarke.com/api/"
